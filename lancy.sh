dd6dd309b3240b9dbaa16c15b8f1db6b1b27570d
weiguo zhao <wgzhao@gmail.com>
2011-08-24 22:25:26 +0800

    删除一些过期的脚本，统一增加脚本后缀

--- lancy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo 1 >/proc/sys/net/ipv4/ip_forward
-/etc/init.d/iptables start
-iptables -A POSTROUTING -t nat   -o eth0 -j SNAT --to-source  10.10.116.173
-/etc/init.d/iptables save
-
--- pronounce-word.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-TTS_PATH=/usr/share/WyabdcRealPeopleTTS
-WORD=`echo $* | tr A-Z a-z`
-for WORD2 in $WORD
-do
-    play ${TTS_PATH}/${WORD2:0:1}/${WORD2}.wav
-done
