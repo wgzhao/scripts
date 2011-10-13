dd5a806ff929fed510d6cdb0c3f7b2eb50e3b2c7
weiguo zhao <wgzhao@gmail.com>
2011-10-13 15:36:09 +0800

    kill最老的数据库空连接

---
+++ killconns.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#kill oldest 30 idle connections 
+#author wgzhao (wgzhao@kingbase.com.cn)
+
+#connection limit
+connlimit=7
+#connections will be killed
+killconns=5
+#kingbase connnection
+user="SYSTEM"
+pswd="kb613"
+servername="192.168.100.103"
+dbname="KB613"
+querystr="select procpid,current_query from sys_stat_activity order by backend_start asc;"
+ISQL="/usr/local/kingbase/bin/isql"
+output="/var/tmp/$$.txt"
+
+#query current all connections and status
+$ISQL  -t -o $output -h "$servername" -U "$user" -W "$pswd" -c "$querystr" "$dbname"
+if [ $? -gt 0 ];then
+	echo "connection errror"
+	exit 2
+fi
+
+conns=$(wc -l $output |awk '{print $1}')
+if [ $conns -le $connlimit ];then
+	echo "it's ok"
+	exit 0
+fi
+
+#kill some idle connections
+export KILLCONNS=$killconns
+ids=$(awk -F'|' 'BEGIN{COUNTER=0;ORS=" "}{if ( toupper($2)  ==   " <IDLE>" && COUNTER < ENVIRON["KILLCONNS"] )  {print $1;COUNTER+=1}}' $output)
+sql="/var/tmp/killsession.sql"
+> $sql
+for i in $ids
+do
+	echo "alter system kill session $i;" >>$sql
+done
+
+$ISQL  -t -o $output -h "$servername" -U "$user" -W "$pswd" -f "$sql" "$dbname"
+#cleanup
+rm -f $output
+rm -f $sql
+
+exit $?
+
