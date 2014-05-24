7b627f985dc9b19be3e61fe4afa35b655196d3f8
wgzhao <wgzhao@gmail.com>
2014-05-24 10:54:19 +0800

    change document root path

--- addsite.sh
+++ addsite.sh
@@ -3,18 +3,19 @@
 # the script only test on mac os x
 # $1 domain such like www.example.com
 [ -z $1 ] && exit 1
-apachefile="/etc/apache2/extra/http-vhosts.conf"
+apachefile="/etc/apache2/extra/httpd-vhosts.conf"
 wpversion="3.9"
 domain=$1
-
+wwwroot="/opt/websites"
 cat - >> $apachefile <<EOF
 <VirtualHost *:80>
     ServerAdmin webmaster@$domain
-    DocumentRoot "/Users/weiguo/Sites/$domain/"
+    DocumentRoot "$wwwroot/$domain/"
     ServerName  $domain
-    <Directory "/Users/weiguo/Sites/$domain/">
+    <Directory "$wwwroot/$domain/">
         Options +FollowSymLinks
         AllowOverride All
+		Allow from All
     </Directory>
     ErrorLog "/private/var/log/apache2/$domain-error_log"
     CustomLog "/private/var/log/apache2/$domain-access_log" common
@@ -23,14 +24,15 @@ EOF
 
 echo "127.0.0.1    $domain" >>/etc/hosts
 
-if [ ! -d ~weiguo/Sites/$domain ];then
-  mkdir ~weiguo/Sites/$domain 
-  cd ~weiguo/Sites/$domain
-  unzip  ../wordpress-${wpversion}-zh_CN.zip >/dev/null 2>&1
+if [ ! -d $wwwroot/$domain ];then
+  mkdir -p $wwwroot/$domain 
+  cd $wwwroot/$domain
+  wget -q -O wordpress.zip http://cn.wordpress.org/wordpress-${version}-zh_CN.zip
+  unzip -q wordpress.zip
   mv wordpress/* .
   rm -r wordpress
   cd -
-  chmod -R 777 ~weiguo/Sites/$domain
+  chmod -R 777 $wwwroot/$domain
 fi
 
 launchctl load /System/Library/LaunchDaemons/org.apache.httpd.plist
