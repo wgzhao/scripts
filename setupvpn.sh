aa93bb492136a301ec05df3b5f499c5b205c4a4e
wgzhao <wgzhao@gmail.com>
2014-08-27 21:41:20 +0800

    add firewall rules and ip forward

--- setupvpn.sh
+++ setupvpn.sh
@@ -44,11 +44,17 @@ do
 	;;
 	esac
 done
-
-os=$(head -n1 /etc/issue 2>/dev/null |awk '{print $1}' )
-if [ "$os" = "CentOS" -o "$os" = "RedHat" ];then
+if [ $uid -ne 0 ];then
+	echo "setup vpn need root privileges"
+	echo "you can aslo execute with sudo ,if your account has sudo privilege"
+	exit 1
+fi
+osdetect=$(head -n1 /etc/issue 2>/dev/null |awk '{print $1}' )
+if [ "$osdetect" = "CentOS" -o "$osdetect" = "Red" ];then
+	ostype="Redhat"
     cmd="yum install -y "
-elif [ "$os" = "Debian" -o "$os"  = "Ubuntu" ];then
+elif [ "$osdetect" = "Debian" -o "$osdetect"  = "Ubuntu" ];then
+	ostype="Debian"
     cmd="apt-get -y install "
 else
     echo "Unsupported OS distribution"
@@ -70,9 +76,18 @@ fi
 #setup pptp
 
 echo "
-localip=$localip
-remoteip=$remoteip
+localip $localip
+remoteip $remoteip
 " >>/etc/pptpd.conf
+
+# add dns configure 
+if [ "$ostype"  = "Redhat" ];then
+	optionsfile="/etc/ppp/options.pptpd"
+else
+	optionsfile="/etc/ppp/pptpd-options"
+fi
+
+sed -i '13i ms-dns 8.8.8.8\nms-dns 8.8.4.4' $optionsfile
 
 # add account to chap file
 echo -e "$username \t pptpd \t $password \t * " >> /etc/ppp/chap-secrets
@@ -80,6 +95,27 @@ echo -e "$username \t pptpd \t $password \t * " >> /etc/ppp/chap-secrets
 #start pptpd service
 service pptpd start
 
+#set ip forward
+echo "net.ipv4.ip_foward=1" >>/etc/sysctl.conf
+sysctl -p >/dev/null 2>&1
+
+#set iptables rules
+if [ "$ostype" = "Redhat" ];then
+	/sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+	/sbin/iptables -o eth0 -A FORWARD -p tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 800:1536 -j TCPMSS --clamp-mss-to-pmtu
+	service iptables save
+else
+	#save to a script
+	echo "#!/bin/sh
+	IPT='/sbin/iptables'
+
+	$IPT -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+	$IPT -o eth0 -A FORWARD -p tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 800:1536 -j TCPMSS --clamp-mss-to-pmtu
+	" > /etc/network/if-up.d/pptp.sh
+	chmod +x /etc/network/if-up.d/pptp.sh
+	#execute it
+	/etc/network/if-up.d/pptp.sh
+fi
 # print account information
 echo "Now,you can connect your VPN(PPTP) username: $username and password: $password . enjoy it"
 exit 0
