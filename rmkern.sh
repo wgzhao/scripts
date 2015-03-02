a4c0f7e021ef43faeb55b8026ce4182c091fe480
wgzhao <zhaoweiguo@haodou.com>
2015-03-02 14:34:27 +0800

    remove unused scripts

--- rmkern.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#remove all unused kernel image packages,as well as headers packages correspendly
-sudo aptitude remove $(dpkg -l linux-image* linux-headers* |grep ^ii |grep -v `uname -r` |awk '{print $2}')
-
